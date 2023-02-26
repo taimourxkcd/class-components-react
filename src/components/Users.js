30f32fcd90621f94d775828e4f642808f82fc246
taimourz <taimour.rkt@gmail.com>
2023-02-26 23:01:45 +0500

    added the ErrorBoundry file. it has bugs but i am too lazy to find them

--- src/components/Users.js
+++ src/components/Users.js
@@ -1,21 +1,30 @@
-import { Component } from "react/cjs/react.development";
+import { Component } from "react";
+
 import User from "./User";
-
 import classes from "./Users.module.css";
-
-
 
 class Users extends Component {
   constructor() {
     super();
-
     this.state = {
       showUsers: true,
       more: "Test",
     };
   }
+
+  componentDidUpdate() {
+    // try {
+    //   someCodeWhichMightFail()
+    // } catch (err) {
+    //   // handle error
+    // }
+    if (this.props.users.length === 0) {
+      throw new Error("No users provided!");
+    }
+  }
+
   toggleUsersHandler() {
-    // this.state.showUsers = false // NOT
+    // this.state.showUsers = false; // NOT!
     this.setState((curState) => {
       return { showUsers: !curState.showUsers };
     });
@@ -29,6 +38,7 @@ class Users extends Component {
         ))}
       </ul>
     );
+
     return (
       <div className={classes.users}>
         <button onClick={this.toggleUsersHandler.bind(this)}>
@@ -40,4 +50,29 @@ class Users extends Component {
   }
 }
 
-export default Users
+// const Users = () => {
+//   const [showUsers, setShowUsers] = useState(true);
+
+//   const toggleUsersHandler = () => {
+//     setShowUsers((curState) => !curState);
+//   };
+
+//   const usersList = (
+//     <ul>
+//       {DUMMY_USERS.map((user) => (
+//         <User key={user.id} name={user.name} />
+//       ))}
+//     </ul>
+//   );
+
+//   return (
+//     <div className={classes.users}>
+//       <button onClick={toggleUsersHandler}>
+//         {showUsers ? 'Hide' : 'Show'} Users
+//       </button>
+//       {showUsers && usersList}
+//     </div>
+//   );
+// };
+
+export default Users;
